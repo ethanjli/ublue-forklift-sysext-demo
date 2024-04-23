de1fec6b610e02420fa4978f0a37e9d190b81483
Ethan Li <lietk12@gmail.com>
2024-04-23 12:28:56 -0700

    Fix syntax error

--- build_files/forklift-extensions/build.sh
+++ build_files/forklift-extensions/build.sh
@@ -10,7 +10,7 @@ ARCH="$(arch)"
 FORKLIFT_VERSION="0.7.0-alpha.3"
 # Do we need any other substitutions? We use goreleaser's defaults (e.g. `arm64` for 64-bit ARM, and
 # `arm` for armv7); it probably doesn't matter because for now ublue only has amd64 builds:
-FORKLIFT_ARCH=(echo "$ARCH" | sed -e 's/x86_64/amd64/')
+FORKLIFT_ARCH="$(echo "$ARCH" | sed -e 's/x86_64/amd64/')"
 curl -L "https://github.com/PlanktoScope/forklift/releases/download/v$FORKLIFT_VERSION/forklift_${FORKLIFT_VERSION}_linux_${FORKLIFT_ARCH}.tar.gz" \
   | sudo tar -C /usr/bin -xz forklift
 sudo mv /usr/bin/forklift "/usr/bin/forklift-${forklift_version}"
