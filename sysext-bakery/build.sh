70302cef577b84deeb008f9e22a1ed93ebc774df
Ethan Li <lietk12@gmail.com>
2024-05-15 01:52:26 -0700

    Mount lynx's `/etc` in the sysext chroot

--- sysext-bakery/build.sh
+++ sysext-bakery/build.sh
@@ -6,5 +6,6 @@ CMD="apk -U add neovim neovim-doc" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-neo
 OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-neovim neovim /usr/bin/nvim
 
 echo "Building lynx sysext..."
-CMD="apk -U add lynx" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-lynx
+# lynx needs /etc/lynx.cfg, which we can just provide from a chrooted /etc:
+ETCMAP=chroot CMD="apk -U add lynx" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-lynx
 OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-lynx lynx /usr/bin/lynx
