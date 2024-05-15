a4496db29d42df8a15d97ceeb9d2cae48f1c3f11
Ethan Li <lietk12@gmail.com>
2024-05-15 00:31:48 -0700

    Try to run `oci-rootfs.sh` and `flatwrap.sh` for w3m sysext

--- sysext-bakery/build.sh
+++ sysext-bakery/build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Hello, world!"
+echo "Building w3m sysext..."
+CMD="apk -U add w3m" ./oci-rootfs.sh docker.io/library/alpine:3.19.1 /tmp/alpine-w3m
+OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-w3m w3m /usr/bin/w3m
+ls
