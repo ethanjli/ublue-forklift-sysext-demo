45ba8e5b0e4a432b0e06b0deb2a01de2fb4fb25d
Ethan Li <lietk12@gmail.com>
2024-05-15 01:13:19 -0700

    Try to work around w3m terminfo issues

--- sysext-bakery/build.sh
+++ sysext-bakery/build.sh
@@ -3,4 +3,6 @@ set -euo pipefail
 
 echo "Building w3m sysext..."
 CMD="apk -U add w3m" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-w3m
-OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-w3m w3m /usr/bin/w3m
+# /etc provides terminfo files needed by w3m which are not available on the host, so we map /etc
+# into the chroot:
+ETCMAP="chroot" OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-w3m w3m /usr/bin/w3m
