e05ec4994349f837f5bb703ab3002289c5a40847
Ethan Li <lietk12@gmail.com>
2024-05-15 01:21:45 -0700

    Build neovim instead of w3m (w3m somehow can't open network sockets)

--- sysext-bakery/build.sh
+++ sysext-bakery/build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Building w3m sysext..."
-CMD="apk -U add w3m" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-w3m
-# /etc provides terminfo files needed by w3m which are not available on the host, so we map /etc
-# into the chroot:
-ETCMAP="chroot" OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-w3m w3m /usr/bin/w3m
+echo "Building neovim sysext..."
+CMD="apk -U add neovim" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-neovim
+OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-neovim neovim /usr/bin/nvim
