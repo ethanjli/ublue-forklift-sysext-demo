31e2f300f65197294cdca860d81cccec6b738921
Ethan Li <lietk12@gmail.com>
2024-05-15 11:01:51 -0700

    Try setting up a GitHub Actions workflow to build a sysext for bluefin-dx's fonts, as an OCI image

--- sysext-bakery/build.sh
+++ sysext-bakery/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Building neovim sysext..."
+mkdir -p build
+
+echo "Building neovim sysext as .raw image..."
 CMD="apk -U add neovim neovim-doc" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-neovim
 OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-neovim neovim /usr/bin/nvim
-
-echo "Building go sysext..."
-./oci-rootfs.sh chainguard/go:latest /tmp/wolfi-go
-OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/wolfi-go go /usr/lib/go/bin/go:/usr/bin/go /usr/lib/go/bin/gofmt:/usr/bin/gofmt
+# TODO: make flatwrap keep the files around in the build directory so that we can copy them into a
+# container
