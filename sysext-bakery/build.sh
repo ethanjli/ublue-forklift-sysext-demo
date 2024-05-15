f43ec7761c42518a314a072b840bcfc549e13fa6
Ethan Li <lietk12@gmail.com>
2024-05-15 09:23:19 -0700

    Try to fix issue in pulling chainguard's go image with `oci-rootfs`

--- sysext-bakery/build.sh
+++ sysext-bakery/build.sh
@@ -6,5 +6,5 @@ CMD="apk -U add neovim neovim-doc" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-neo
 OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-neovim neovim /usr/bin/nvim
 
 echo "Building go sysext..."
-./oci-rootfs.sh cgr.dev/chainguard/go:latest /tmp/wolfi-go
+REGISTRY="cgr.dev" ./oci-rootfs.sh chainguard/go:latest /tmp/wolfi-go
 OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/wolfi-go go /usr/lib/go/bin/go:/usr/bin/go /usr/lib/go/bin/gofmt:/usr/bin/gofmt
