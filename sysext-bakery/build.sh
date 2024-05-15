e932b6af30bcb682fa7ace96b39780e536fd6cce
Ethan Li <lietk12@gmail.com>
2024-05-15 09:09:42 -0700

    Try to build a go sysext from wolfi

--- sysext-bakery/build.sh
+++ sysext-bakery/build.sh
@@ -5,6 +5,6 @@ echo "Building neovim sysext..."
 CMD="apk -U add neovim neovim-doc" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-neovim
 OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-neovim neovim /usr/bin/nvim
 
-echo "Building bluefin-cli sysext..."
-CMD="" ./oci-rootfs.sh ghcr.io/ublue-os/bluefin-cli:latest /tmp/wolfi-bluefin-cli
-OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/wolfi-bluefin-cli bluefin-cli /usr/bin/atuin /usr/bin/delta /usr/bin/gawk /usr/bin/eza /usr/bin/fd /usr/bin/fish /usr/bin/fzf /usr/bin/rclone /usr/bin/ripgrep /usr/bin/sed /usr/bin/starship /usr/bin/zoxide /bin/zsh:/usr/bin/zsh /usr/share/bash-prexec:/usr/sharepbash-prexec
+echo "Building go sysext..."
+./oci-rootfs.sh cgr.dev/chainguard/go:latest /tmp/wolfi-go
+OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/wolfi-go go /usr/lib/go/bin/go:/usr/bin/go /usr/lib/go/bin/gofmt:/usr/bin/gofmt
