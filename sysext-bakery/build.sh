195d31062472ecfa55a260168e1058599d7ab605
Ethan Li <lietk12@gmail.com>
2024-05-15 01:26:30 -0700

    Try to add neovim-doc to the neovim sysext

--- sysext-bakery/build.sh
+++ sysext-bakery/build.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 echo "Building neovim sysext..."
-CMD="apk -U add neovim" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-neovim
+CMD="apk -U add neovim neovim-doc" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-neovim
 OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-neovim neovim /usr/bin/nvim
