84784b1fa10af3c821485a5d43f3a88ca18c3670
Ethan Li <lietk12@gmail.com>
2024-05-15 01:45:56 -0700

    Try to build a lynx sysext

--- sysext-bakery/build.sh
+++ sysext-bakery/build.sh
@@ -4,3 +4,7 @@ set -euo pipefail
 echo "Building neovim sysext..."
 CMD="apk -U add neovim neovim-doc" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-neovim
 OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-neovim neovim /usr/bin/nvim
+
+echo "Building lynx sysext..."
+CMD="apk -U add lynx" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-lynx
+OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-lynx lynx /usr/bin/lynx
