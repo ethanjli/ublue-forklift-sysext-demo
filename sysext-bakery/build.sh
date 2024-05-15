495439bb32d8703357445baf456474e627b4ea91
Ethan Li <lietk12@gmail.com>
2024-05-15 00:34:11 -0700

    Fix incorrect argument to `oci-rootfs.sh`

--- sysext-bakery/build.sh
+++ sysext-bakery/build.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 echo "Building w3m sysext..."
-CMD="apk -U add w3m" ./oci-rootfs.sh docker.io/library/alpine:3.19.1 /tmp/alpine-w3m
+CMD="apk -U add w3m" REGISTRY="docker.io" ./oci-rootfs.sh library/alpine:3.19.1 /tmp/alpine-w3m
 OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-w3m w3m /usr/bin/w3m
 ls
