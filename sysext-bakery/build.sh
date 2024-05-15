94e85040bcc8648173afa10b70c97a146c6ad87a
Ethan Li <lietk12@gmail.com>
2024-05-15 09:09:28 -0700

    Don't build lynx (it somehow can't connect to internet, maybe because of network configs in /etc)

--- sysext-bakery/build.sh
+++ sysext-bakery/build.sh
@@ -5,7 +5,6 @@ echo "Building neovim sysext..."
 CMD="apk -U add neovim neovim-doc" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-neovim
 OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-neovim neovim /usr/bin/nvim
 
-echo "Building lynx sysext..."
-# lynx needs /etc/lynx.cfg, which we can just provide from a chrooted /etc:
-CMD="apk -U add lynx" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-lynx
-ETCMAP=chroot OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-lynx lynx /usr/bin/lynx
+echo "Building bluefin-cli sysext..."
+CMD="" ./oci-rootfs.sh ghcr.io/ublue-os/bluefin-cli:latest /tmp/wolfi-bluefin-cli
+OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/wolfi-bluefin-cli bluefin-cli /usr/bin/atuin /usr/bin/delta /usr/bin/gawk /usr/bin/eza /usr/bin/fd /usr/bin/fish /usr/bin/fzf /usr/bin/rclone /usr/bin/ripgrep /usr/bin/sed /usr/bin/starship /usr/bin/zoxide /bin/zsh:/usr/bin/zsh /usr/share/bash-prexec:/usr/sharepbash-prexec
