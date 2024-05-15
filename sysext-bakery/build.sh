8ac4911e3b22afb570cd7d8d1acb9173cad9b389
Ethan Li <lietk12@gmail.com>
2024-05-15 00:39:30 -0700

    Try to upload sysext images in workflow runs

--- sysext-bakery/build.sh
+++ sysext-bakery/build.sh
@@ -4,4 +4,3 @@ set -euo pipefail
 echo "Building w3m sysext..."
 CMD="apk -U add w3m" ./oci-rootfs.sh alpine:3.19.1 /tmp/alpine-w3m
 OS="_any" ARCH="" RELOAD="0" ./flatwrap.sh /tmp/alpine-w3m w3m /usr/bin/w3m
-ls
